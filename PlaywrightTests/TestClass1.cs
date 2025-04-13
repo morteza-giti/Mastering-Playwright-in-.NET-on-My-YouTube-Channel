41b66c0532089ca2bb9f3728c23be02916072940
Pirooz <mortezagiti1990@gmail.com>
2025-04-13 06:35:45 -0400

    end of episode 1

---
+++ PlaywrightTests/TestClass1.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace PlaywrightTests;
+
+public class TestClass1 : PageTest
+{
+    [Fact]
+    public async Task HasTitle()
+    {
+        // Expect a title "to contain" a substring.
+        await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
+
+    [Fact]
+    public async Task GetStartedLink()
+    {
+        await Page.GetByRole(AriaRole.Link, new PageGetByRoleOptions() { Name = "Get started" }).ClickAsync();
+
+        await Expect(Page.GetByRole(AriaRole.Heading, new PageGetByRoleOptions() { Name = "Installation" })).ToBeVisibleAsync();
+    }
+}
